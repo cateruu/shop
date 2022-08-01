f8edc96f00ecc20bb1bef0d76c49434f21704c51
Paweł Kromołowski <cater981pl@gmail.com>
2022-08-01 16:46:33 +0200

    background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import styles from './styles/app.module.css';
+import Navbar from './components/Navbar';
+import { ReactComponent as Blob } from './images/blob.svg';
 
 function App() {
-  return <main></main>;
+  return (
+    <main className={styles.main}>
+      <Blob className={styles.blob} />
+      <Navbar />
+    </main>
+  );
 }
 
 export default App;
