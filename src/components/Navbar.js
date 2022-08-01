a40512a131eb817a6ff3ec4f8bee74a6dfea988d
Paweł Kromołowski <cater981pl@gmail.com>
2022-08-01 17:29:13 +0200

    search input

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+import styles from './styles/navbar.module.css';
+import { BsBag } from 'react-icons/bs';
+
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  return (
+    <nav className={styles.nav}>
+      <h1 className={styles.name}>Fruits</h1>
+      <div className={styles.container}>
+        <input
+          type='text'
+          placeholder='Search'
+          onChange={handleChange}
+          className={styles.input}
+          value={searchInput}
+        />
+        <BsBag className={styles.cart} />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
