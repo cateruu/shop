99f4dec0a5fffcd55e84dd02a7f60c05c7129ac9
Paweł Kromołowski <cater981pl@gmail.com>
2022-08-02 16:46:52 +0200

    product tile design

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,34 @@
 import styles from './styles/products.module.css';
 import products from '../products.json';
+import Slider from 'react-slick';
+import { IoAdd } from 'react-icons/io5';
+import { IconContext } from 'react-icons';
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+};
 
 const Products = () => {
   const productElements = products.map((product) => {
-    return <div key={product.id}>{product.name}</div>;
+    return (
+      <div key={product.id} className={styles.product}>
+        <img src={product.image} alt={product.name} className={styles.image} />
+        <div className={styles.description}>
+          <h3 className={styles.name}>{product.name}</h3>
+          <p className={styles.desc}>{product.description}</p>
+          <p className={styles.price}>${product.price}</p>
+          <div className={styles.add}>
+            <IconContext.Provider value={{ className: styles.addIcon }}>
+              <IoAdd />
+            </IconContext.Provider>
+          </div>
+        </div>
+      </div>
+    );
   });
 
   return <section className={styles.container}>{productElements}</section>;
