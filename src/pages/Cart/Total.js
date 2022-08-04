46ea289731c7705e0d2c4f55ba0366f06520fa9d
Paweł Kromołowski <cater981pl@gmail.com>
2022-08-04 19:13:16 +0200

    buy & clear button

--- src/pages/Cart/Total.js
+++ src/pages/Cart/Total.js
@@ -4,9 +4,15 @@ import { useSelector } from 'react-redux';
 const Total = () => {
   const { total } = useSelector((state) => state.cart);
   return (
-    <section className={styles.total}>
-      <h3 className={styles.header}>Total</h3>
-      <p className={styles.price}>${total.toFixed(2)}</p>
+    <section>
+      <div className={styles.total}>
+        <h3 className={styles.header}>Total</h3>
+        <p className={styles.price}>${total.toFixed(2)}</p>
+      </div>
+      <div>
+        <button className={`${styles.buy} ${styles.btn}`}>Buy</button>
+        <button className={`${styles.clear} ${styles.btn}`}>Clear</button>
+      </div>
     </section>
   );
 };
